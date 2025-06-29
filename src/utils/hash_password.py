cd2e028f52f6b84802b332484127f4b036ae1270
Shiva Adhikari <s2whl6xh@duck.com>
2025-06-29 07:57:01 +0545

    feat: convert text to hash password

---
+++ src/utils/hash_password.py
@@ -0,0 +1,14 @@
+# Third-party imports
+from passlib.context import CryptContext
+
+
+pwd_context: CryptContext = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+# Encrypt password
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
