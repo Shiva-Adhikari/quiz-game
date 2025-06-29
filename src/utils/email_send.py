c9c2e314db991e95789a538e06ba9f4d28d0a1b8
Shiva Adhikari <s2whl6xh@duck.com>
2025-06-29 07:55:52 +0545

    feat: send otp to verify email

---
+++ src/utils/email_send.py
@@ -0,0 +1,35 @@
+# Standard library imports
+import ssl
+import smtplib
+
+# Local imports
+from email.message import EmailMessage
+from src.core.config import settings
+
+
+def send_email(email_receiver: str, otp: int) -> bool:
+    html_content = f'get {otp}'
+    em = EmailMessage()
+    em['From'] = settings.SENDER_EMAIL
+    em['To'] = email_receiver
+    em['Subject'] = 'Verify Email'
+    em.set_content(html_content, subtype='html')
+
+    # establish tls/ssl connection
+    context = ssl.create_default_context()
+
+    try:
+        # with help to properly close connection
+        with smtplib.SMTP_SSL(
+                settings.EMAIL_HOST.get_secret_value(),
+                settings.EMAIL_PORT, context=context) as smtp:
+            smtp.login(
+                settings.SENDER_EMAIL,
+                settings.SENDER_PASSWORD.get_secret_value())
+            smtp.sendmail(
+                settings.SENDER_EMAIL, email_receiver, em.as_string())
+        return True
+    except smtplib.SMTPRecipientsRefused:
+        return False    # Email not Valid
+    except Exception:
+        return False    # Exception occured
