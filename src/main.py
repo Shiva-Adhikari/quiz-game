2a67159c2f725eb2fafccc290a861e2a20781450
Shiva Adhikari <s2whl6xh@duck.com>
2025-07-23 18:39:28 +0545

    feat: add question

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ from fastapi import FastAPI
 # Local imports
 from src.core.config import settings
 from src.core.database import create_tables
-from src.api.user import router as user_router
+from src.api.authentication import router as authentication_router
+from src.api.questions import router as question_router
+# from src.api.user_data import router as user_data_router
+# from src.api.analytics import router as analytics_router
+# from src.api.start_quiz import router as start_random_quiz_router
 
 
 create_tables()
@@ -22,7 +26,11 @@ def root():
 
 
 # Include routers
-app.include_router(user_router, prefix='/api/v1')
+app.include_router(authentication_router, prefix='/api/v1')
+app.include_router(question_router, prefix='/api/v1')
+# app.include_router(user_data_router, prefix='/api/v1')
+# app.include_router(analytics_router, prefix='/api/v1')
+# app.include_router(start_random_quiz_router, prefix='/api/v1')
 
 if __name__ == '__main__':
     import uvicorn
