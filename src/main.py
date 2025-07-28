6ab5ac78edc292a1747bfe151be93c13353e271d
Shiva Adhikari <s2whl6xh@duck.com>
2025-07-28 16:27:40 +0545

    feat: add route for category wise quiz

--- src/main.py
+++ src/main.py
@@ -6,11 +6,8 @@ from src.core.config import settings
 from src.core.database import create_tables
 from src.api.authentication import router as authentication_router
 from src.api.questions import router as question_router
-from src.api.start_quiz import router as start_random_quiz_router
-
-
-# from src.api.user_data import router as user_data_router
-# from src.api.analytics import router as analytics_router
+from src.api.quiz_random import router as start_random_quiz_router
+from src.api.quiz_category import router as start_category_quiz_router
 
 
 create_tables()
@@ -31,11 +28,7 @@ def root():
 app.include_router(authentication_router, prefix='/api/v1')
 app.include_router(question_router, prefix='/api/v1')
 app.include_router(start_random_quiz_router, prefix='/api/v1')
-
-'''
-# app.include_router(user_data_router, prefix='/api/v1')
-# app.include_router(analytics_router, prefix='/api/v1')
-'''
+app.include_router(start_category_quiz_router, prefix='/api/v1')
 
 
 if __name__ == '__main__':
